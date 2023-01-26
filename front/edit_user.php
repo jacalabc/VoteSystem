fab9ce01f89e12f1578c511b23bdb7cfd3f1c3f6
Jerry <jerry19971228@gmail.com>
2023-01-26 16:49:48 +0800

    完成會員資料修改功能

--- front/edit_user.php
+++ front/edit_user.php
@@ -1,5 +1,14 @@
 <?php
 include_once "./api/base.php";
+
+           // 找acc欄位
+            $acc = array_column($Users->all(), 'acc');
+            // print_r($acc);
+            // 找id欄位
+            $id = array_column($Users->all(),'id');
+            // print_r($id);
+            // 找key值
+            $key = array_search($_SESSION['login'],$acc);
 ?>
 <div class="container-fluid mt-3">
     <div class="text-center">
@@ -7,17 +16,14 @@ include_once "./api/base.php";
     </div>
 
     <div class="container col-3">
+    <div class="form-floating mb-3">
+            <input type="hidden" class="form-control" name="id" id="id" value="<?=array_column($Users->all(), 'id')[$key];?>">
+            <label for="id">ID</label>
+        </div>
         <div class="form-floating mb-3">
             <input type="text" class="form-control" name="acc" id="acc" value="<?=$_SESSION['login'];?>" placeholder="填寫你的帳號:D">
             <label for="acc">帳號</label>
         </div>
-       <?php
-            // 找acc欄位
-            $acc = array_column($Users->all(), 'acc');
-            // print_r($acc);
-            // 找key值
-            $key = array_search($_SESSION['login'],$acc);  
-        ?>
         <div class="form-floating mb-3">
             <input type="password" class="form-control" name="pw" id="pw" value="<?=array_column($Users->all(), 'pw')[$key];?>" placeholder="填寫你的密碼:D">
             <label for="pw">密碼</label>
@@ -33,7 +39,7 @@ include_once "./api/base.php";
     </div>
     <div class="text-center">
         <button class="btn btn-outline-info me-5" onclick='reset()'>清除</button>
-        <button class="btn btn-outline-success" onclick='reg()'>修改</button>
+        <button class="btn btn-outline-success" onclick='edit()'>修改</button>
     </div>
 </div>
 
@@ -42,14 +48,15 @@ include_once "./api/base.php";
         $("#acc,#pw,#pw2,#email").val('')
     }
 
-    function reg() {
+    function edit() {
         let user = {
             acc: $("#acc").val(),
             pw: $("#pw").val(),
             pw2: $("#pw2").val(),
-            email: $("#email").val(),           
+            email: $("#email").val(),
+            id: $('#id').val()           
         }
-
+        console.log(user.id)
         let EmailCharCheck = user.email.split("@")[1];
         if (user.acc === '' || user.pw === '' || user.pw2 === '' || user.email === '') {
             //有空白
@@ -86,7 +93,7 @@ include_once "./api/base.php";
                                 Swal.fire({
                                     icon: 'success',
                                     title: '溫馨提示',
-                                    text: '修改完成'
+                                    text: '修改完成，請重新登入以更新畫面:D'
                                 })
                                 reset();
                             })
